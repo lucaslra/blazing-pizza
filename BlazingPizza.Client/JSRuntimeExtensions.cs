0483f1c2ec3b4184446874ca7937d4e3fcc1c032
Lucas Araujo <lucas.de.ribeiroaraujo@asml.com>
2020-06-17 21:11:28 +0200

    JS runtime integration

---
+++ BlazingPizza.Client/JSRuntimeExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace BlazingPizza.Client
+{
+    public static class JSRuntimeExtensions
+    {
+        public static ValueTask<bool> Confirm(this IJSRuntime jsRuntime, string message) => jsRuntime.InvokeAsync<bool>("confirm", message);
+    }
+}
